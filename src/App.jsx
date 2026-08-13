cab769c47401661d8876961f586c253dc71db7a7
tripng <triwahyu.png@gmail.com>
2026-08-14 07:57:23 +0800

    Fix: import useEffect in App (runtime crash)

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useLenisScroll } from "./hooks/useLenisScroll";
 import LoaderCurtain from "./components/LoaderCurtain";
