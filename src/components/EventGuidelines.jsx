2ec229d1133817d1daa91221bf631f7ffc3a96d5
tripng <triwahyu.png@gmail.com>
2026-08-14 14:48:30 +0800

    EventGuidelines: drop unused ScrollTrigger/guidelineStagger imports

--- src/components/EventGuidelines.jsx
+++ src/components/EventGuidelines.jsx
@@ -1,9 +1,7 @@
 import { useEffect, useRef } from "react";
 import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import {
   guidelineRevealDuration,
-  guidelineStagger,
   swatchPop,
 } from "../constants/animationConfig";
 
